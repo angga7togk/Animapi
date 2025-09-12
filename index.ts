0615366c61de64c3321f71124765f9add792647e
Angga7Togk <kiplihode123321@gmail.com>
2025-09-13 00:58:26 +0700

    [anime-indo] add new provider

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from "./provider";
 export * from "./provider/animasu";
 export * from "./provider/otakudesu";
+export * from "./provider/anime-indo";
 
 export * from "./utils";
 export * from "./types";
